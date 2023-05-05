fb1a5836d0887ac0e6249a8d0c0d2a5910c1eeae
HeloBaron <helo.athayde@gmail.com>
2023-05-05 17:53:29 -0300

    feat: Create carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
+
+import { useKeenSlider } from 'keen-slider/react'
+
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import logo from '../assets/logo.svg'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [slideRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={slideRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={logo} width={520} height={480} alt=""/>
 
         <footer>
@@ -15,7 +26,25 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={logo} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={logo} width={520} height={480} alt=""/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      
+      <Product className="keen-slider__slide">
         <Image src={logo} width={520} height={480} alt=""/>
 
         <footer>
