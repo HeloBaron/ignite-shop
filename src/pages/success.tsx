f4fd33f3f2f5c806f9895dd7c8d738fde33fb015
HeloBaron <helo.athayde@gmail.com>
2023-06-02 17:32:34 -0300

    update: Success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,25 +1,68 @@
 import Link from "next/link";
 import { ImageContainer } from "../styles/pages/product";
 import { SuccessContainer } from "../styles/pages/success";
+import { GetServerSideProps } from "next";
+import { stripe } from "../lib/stripe";
+import Stripe from "stripe";
+import Image from "next/image";
 
-export default function Success() {
+interface SuccessProps{
+    customerName: string;
+    product: {
+        name: string;
+        imageUrl: string;
+    }
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
     return (
         <SuccessContainer>
-            <h1>Compra efetuada!</h1>
+        <h1>Compra efetuada</h1>
 
-            <ImageContainer>
-                
-            </ImageContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt="" />
+        </ImageContainer>
 
-            <p>
-                Uhuul <strong>Cherry Baron</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
-            </p>
+        <p>
+          Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+        </p>
 
-            <Link href="/">
-                Voltar ao catálogo
-            </Link>
-        </SuccessContainer>
-
+        <Link href="/">
+          Voltar ao catálogo
+        </Link>
+      </SuccessContainer>
         
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    if (!query.session_id) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+    const sessionId = String(query.session_id);
+
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+        expand: ['line_items', 'line_items.data.price.product']
+    })
+
+    console.log(session.line_items.data)
+
+    const customerName = session.customer_details.name;
+    const product = session.line_items.data[0].price.product as Stripe.Product
+
+    return {
+        props: { 
+            customerName,
+            product: {
+                name: product.name,
+                imageUrl: product.images[0]
+            }           
+        }
+    }
 }
--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -7,36 +7,36 @@ export const SuccessContainer = styled('main', {
     justifyContent: 'center',
     margin: '0 auto',
     height: 656,
-
+  
     h1: {
-        fontSize: '$2xl',
-        color: '$gray100',
+      fontSize: '$2xl',
+      color: '$gray100',
     },
-
+  
     p: {
-        fontSize: '$xl',
-        color: '$gray300',
-        maxWidth: 560,
-        textAlign: 'center',
-        marginTop: '2rem',
-        lineHeight: 1.4,
+      fontSize: '$xl',
+      color: '$gray300',
+      maxWidth: 560,
+      textAlign: 'center',
+      marginTop: '2rem',
+      lineHeight: 1.4,
     },
-
+  
     a: {
-        display: 'block',
-        marginTop: '5rem',
-        fontSize: '$lg',
-        color: '$green500',
-        textDecoration: 'none',
-        fontWeight: 'bold',
-
-        '&:hover': {
-            color: '$green300',
-        }
+      display: 'block',
+      marginTop: '5rem',
+      fontSize: '$lg',
+      color: '$green500',
+      textDecoration: 'none',
+      fontWeight: 'bold',
+  
+      '&:hover': {
+        color: '$green300',
+      }
     }
-})
-
-export const ImageContainer = styled('div', {
+  });
+  
+  export const ImageContainer = styled('div', {
     width: '100%',
     maxWidth: 130,
     height: 145,
@@ -44,12 +44,13 @@ export const ImageContainer = styled('div', {
     borderRadius: 8,
     padding: '0.25rem',
     marginTop: '4rem',
-
+  
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-
+  
+  
     img: {
-        objectFit: 'cover',
+      objectFit: 'cover',
     }
-})
+  });
